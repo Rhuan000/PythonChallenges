83fff5b395809dbdf542cf0177c612aff74779ab
Rhuan Elias <86079426+Rhuan000@users.noreply.github.com>
2023-03-19 13:57:13 -0300

    Delete class directory

--- class/nestingDictionary_and_List.py
+++
@@ -1,9 +0,0 @@
-travel_vlog = {
-    "france": {"cities_visited": ["paris", "lille", "Dijon"]},
-    "Germany": ["berlin" , "hamburg", "Stuttgart"]
-}
-
-my_Travel_vlog = {
-    "Rio": {'cities_visited':['macae','arraial','buzios','angra','cabo frio'], "total visits": 5}
-
-}
